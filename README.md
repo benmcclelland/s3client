98fd8af752f361c9bb06b3e8b361a5f0c98eef39
Ben McClelland <ben.mcclelland@versity.com>
2016-09-27 19:14:30 -0700

    working basic client

---
+++ README.md
@@ -0,0 +1,22 @@
+# s3client
+
+s3client is a simple client for uploading and downloading s3 objects using the aws-sdk-go s3manager for high performance concurrent transfers.
+
+The aws-sdk-go that has been vendored in is using the v2 auth modifications found here:
+https://github.com/benmcclelland/aws-sdk-go/tree/ben/s3v2
+
+There is not yet a flag to toggle v2/v4 auth, so for v4 auth just vendor in the aws sdk or remove aws-sdk-go from the vendor directory completely.
+
+This also makes use of the facebookgo flagconfig library that allows specifying option from a config file.  So you can either run it with options like:
+./s3client -filepath=myfile -object=myobject -bucket=mybucket -maxprocs=48 -concurrency=48
+but if you don't want to remember all the options, you can make a config file, myconfig,  that contains:
+filepath=myfile
+object=myobject
+bucket=mybucket
+maxprocs=48
+concurrency=48
+
+and run the command like:
+./s3client -c ./myconfig
+
+you can specify the id and secret on the command line or it will also just pick them up from the environment variables AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY.
---
+++ s3client.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/facebookgo/flagconfig"
+)
+
+type config struct {
+	awsID           string
+	awsSecret       string
+	awsRegion       string
+	bucket          string
+	endpoint        string
+	filePath        string
+	objectPath      string
+	operation       string
+	checksumDisable bool
+	disableSSL      bool
+	pathStyle       bool
+	partSize        int64
+	concurrency     int
+	maxprocs        int
+	debug           bool
+}
+
+func (c *config) getCreds() *credentials.Credentials {
+	// TODO support token/IAM
+	if c.awsID == "" {
+		c.awsID = os.Getenv("AWS_ACCESS_KEY_ID")
+	}
+	if c.awsID == "" {
+		log.Fatal("no AWS_ACCESS_KEY_ID found")
+	}
+	if c.awsSecret == "" {
+		c.awsSecret = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	}
+	if c.awsSecret == "" {
+		log.Fatal("no AWS_SECRET_ACCESS_KEY found")
+	}
+
+	return credentials.NewStaticCredentials(c.awsID, c.awsSecret, "")
+}
+
+func (c *config) getConfig(creds *credentials.Credentials) *aws.Config {
+	config := aws.NewConfig().WithRegion(c.awsRegion).WithCredentials(creds)
+	config = config.WithDisableSSL(c.disableSSL)
+	config = config.WithDisableComputeChecksums(c.checksumDisable)
+	config = config.WithS3ForcePathStyle(c.pathStyle)
+	if c.debug == true {
+		config = config.WithLogLevel(aws.LogDebugWithSigning)
+	}
+	if c.endpoint != "" {
+		config = config.WithEndpoint(c.endpoint)
+	}
+
+	return config
+}
+
+func uploadFile(c *config) {
+	fi, err := os.Lstat(c.filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Open(c.filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	creds := c.getCreds()
+	config := c.getConfig(creds)
+
+	uploader := s3manager.NewUploader(session.New(config))
+	uploader.PartSize = c.partSize
+	uploader.Concurrency = c.concurrency
+
+	start := time.Now()
+
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Body:   file,
+		Bucket: &c.bucket,
+		Key:    &c.objectPath,
+	})
+	elapsed := time.Since(start)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Uploaded:", result.Location, float64(fi.Size()/1048576)/elapsed.Seconds(), "MB/s")
+}
+
+func downloadFile(c *config) {
+	file, err := os.Create(c.filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	creds := c.getCreds()
+	config := c.getConfig(creds)
+
+	downloader := s3manager.NewDownloader(session.New(config))
+	downloader.PartSize = c.partSize
+	downloader.Concurrency = c.concurrency
+
+	start := time.Now()
+
+	// TODO GetObjectInput can to range get
+	size, err := downloader.Download(file, &s3.GetObjectInput{
+		Bucket: &c.bucket,
+		Key:    &c.objectPath,
+	})
+
+	elapsed := time.Since(start)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Downloaded:", c.filePath, float64(size/1048576)/elapsed.Seconds(), "MB/s")
+}
+
+func main() {
+	c := &config{}
+	flag.StringVar(&c.awsID, "id", "", "AWS access key or use env AWS_ACCESS_KEY_ID")
+	flag.StringVar(&c.awsSecret, "secret", "", "AWS secret key or use env AWS_SECRET_ACCESS_KEY")
+	flag.StringVar(&c.operation, "op", "upload", "operation to do upload/download")
+	flag.StringVar(&c.filePath, "filepath", "", "path for local file to read/write")
+	flag.StringVar(&c.objectPath, "object", "", "path for object read/write")
+	flag.StringVar(&c.bucket, "bucket", "", "bucket for target operation")
+	flag.StringVar(&c.endpoint, "endpoint", "", "endpoint if different than s3.amazonaws.com in the form of host:port")
+	flag.StringVar(&c.awsRegion, "region", "us-west-2", "AWS region")
+	flag.BoolVar(&c.checksumDisable, "nocsum", false, "disable checksum for uploads")
+	flag.BoolVar(&c.disableSSL, "nossl", false, "disable https")
+	flag.BoolVar(&c.pathStyle, "pathstyle", false, "force path style requests")
+	flag.Int64Var(&c.partSize, "partsize", 64*1024*1024, "part size for uploads")
+	flag.IntVar(&c.concurrency, "concurrency", 24, "upload concurrency for multipart uploads and downloads")
+	flag.IntVar(&c.maxprocs, "maxprocs", 0, "GOMAXPROCS")
+	flag.BoolVar(&c.debug, "debug", false, "enable debug output")
+	flag.Parse()
+	flagconfig.Parse()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	if c.maxprocs > 0 {
+		runtime.GOMAXPROCS(c.maxprocs)
+	}
+
+	switch c.operation {
+	case "upload":
+		uploadFile(c)
+	case "download":
+		downloadFile(c)
+	default:
+		log.Fatal("operation must be one of upload or download")
+	}
+}
